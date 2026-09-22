1a7eeddba8dfd001a826e8f98ea1ef6ab3ae3e82
pistolydv01-bit <pistolydv01@gmail.com>
2026-09-22 17:37:14 +0530

    Add files via upload

---
+++ sandip.py
@@ -0,0 +1,26 @@
+import os
+import logging
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
+    def log_message(self, format, *args):
+        return  # Logs clean rakhne ke liye
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
+    logging.info(f"🌐 Health Check Server Running on Port {port}")
+    server.serve_forever()
+
+# Background Thread me server start karein:
+threading.Thread(target=run_dummy_server, daemon=True).start()
